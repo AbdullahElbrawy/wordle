2a671e1485c608dbb7cece42b044ede1a5385705
AbdullahElbarawy <abdullahalbrawy00@gmail.com>
2024-06-28 16:16:55 +0200

    main

--- android/app/src/main/java/com/woodlegame/MainApplication.kt
+++ android/app/src/main/java/com/woodlegame/MainApplication.kt
@@ -7,7 +7,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.soloader.SoLoader
 import com.facebook.react.PackageList
 
-abstract class MainApplication : Application(), ReactApplication {
+class MainApplication : Application(), ReactApplication {
 
     private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
         override fun getUseDeveloperSupport(): Boolean {
@@ -23,9 +23,12 @@ abstract class MainApplication : Application(), ReactApplication {
         }
     }
 
+    override val reactNativeHost: ReactNativeHost
+        get() = mReactNativeHost
 
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
     }
 }
+t add
