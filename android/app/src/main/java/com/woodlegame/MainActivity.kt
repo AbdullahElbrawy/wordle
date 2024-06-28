cf892226bda1621ec85b08f7e30e1b02c86959dc
AbdullahElbarawy <abdullahalbrawy00@gmail.com>
2024-06-28 12:20:03 +0200

    main

--- android/app/src/main/java/com/woodlegame/MainActivity.kt
+++ android/app/src/main/java/com/woodlegame/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jkk
+package com.woodlegame
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "jkk"
+  override fun getMainComponentName(): String = "woodlegame"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
